d0a8bca64083faefd178333a1f6be216ee3a7025
nlcus <nalcus@gmail.com>
2019-03-29 18:57:16 -0700

    dadjokes to external file

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ try {
 } catch (ex) {
     var authtoken = process.env.TOKEN;
 }
+// load external data
+var dj = require('./dadjokes.js');
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
@@ -44,21 +46,15 @@ bot.on('message', function (user, userID, channelID, message, evt) {
       // !dadjoke
       // tells a random dad joke
       case 'dadjoke':
-      var jokes=[
-        "A ham sandwich walks into a bar and orders a beer. Bartender says, 'Sorry we don't serve food here.'",
-        "Why do chicken coops only have two doors? Because if they had four, they would be chicken sedans!",
-        "How do you make a Kleenex dance? Put a little boogie in it!",
-        "Two peanuts were walking down the street. One was a salted."
-      ];
-      var numberOfJokes=jokes.length;
+      var numberOfJokes=dadjokes.length;
       var jokeNumber=randomInt(0,numberOfJokes);
-      var joke=jokes[jokeNumber];
+      var joke=dadjokes[jokeNumber];
       bot.sendMessage({
         to: channelID,
         message: joke
       });
       break;
-      case 'advice':
+      case 'dadquotes':
     }
   }
 });
---
+++ dadjokes.js
@@ -0,0 +1,6 @@
+var dadjokes=[
+  "A ham sandwich walks into a bar and orders a beer. Bartender says, 'Sorry we don't serve food here.'",
+  "Why do chicken coops only have two doors? Because if they had four, they would be chicken sedans!",
+  "How do you make a Kleenex dance? Put a little boogie in it!",
+  "Two peanuts were walking down the street. One was a salted."
+];
